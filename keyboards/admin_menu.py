3a4b83bd28c18b3020fad9ec14924d7e1accf674
lirongart <lirongart@gmail.com>
2025-04-13 23:24:35 +0300

    Add files via upload

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -1,13 +1,8 @@
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import ReplyKeyboardMarkup
 
-def admin_panel_inline():
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("הפקדה מ־bit", callback_data="bit_deposit"))
-    markup.add(InlineKeyboardButton("בדיקת יתרות כוללת", callback_data="total_balance"))
-    markup.add(InlineKeyboardButton("ניהול הזמנות", callback_data="manage_orders"))
-    markup.add(InlineKeyboardButton("סיכום כללי", callback_data="summary"))
-    markup.add(
-        InlineKeyboardButton("אספקה גורפת", callback_data="fulfill_all"),
-        InlineKeyboardButton("ביטול כל ההזמנות", callback_data="cancel_all")
-    )
+def admin_main_menu():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row("הפקדה מ־bit", "בדיקת יתרות כוללת")
+    markup.row("ניהול הזמנות", "סיכום כללי")
+    markup.row("אספקה גורפת", "ביטול כל ההזמנות")
     return markup
