6c9cc07a9382d50011ebeade11e47363917c83b1
lirongart <lirongart@gmail.com>
2025-04-13 20:37:23 +0300

    Delete eggbot_pro_v2_bit_ready directory

--- eggbot_pro_v2_bit_ready/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_TELEGRAM_ID", "0"))
-DATABASE_URL = os.getenv("DATABASE_URL")
--- eggbot_pro_v2_bit_ready/keyboards/user_menu.py
+++
@@ -1,7 +0,0 @@
-from telebot.types import ReplyKeyboardMarkup
-
-def main_menu():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row("הזמנת תבניות")
-    markup.row("בדיקת יתרה", "ההזמנות שלי")
-    return markup
