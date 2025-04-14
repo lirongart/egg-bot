61b13cbfe010635e15703067565cec4d25864a65
lirongart <lirongart@gmail.com>
2025-04-14 11:08:14 +0300

    Add files via upload

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -4,7 +4,6 @@ def admin_main_menu():
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row(KeyboardButton("הפקדה מ־bit"))
     markup.row(KeyboardButton("ביטול כל ההזמנות"), KeyboardButton("אספקה גורפת"))
-	markup.row(KeyboardButton("בדיקת יתרות כוללת"), KeyboardButton("סיכום כללי"))
+    markup.row(KeyboardButton("בדיקת יתרות כוללת"), KeyboardButton("סיכום כללי"))
     markup.row(KeyboardButton("פקודות נוספות"), KeyboardButton("ניהול הזמנות"))
     return markup
-
