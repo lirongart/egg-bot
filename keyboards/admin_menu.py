bcbdf8d45bcf50b7d5b3747f28904aec4fe7838d
lirongart <lirongart@gmail.com>
2025-04-16 23:25:36 +0300

    Update admin_menu.py

--- keyboards/admin_menu.py
+++ keyboards/admin_menu.py
@@ -3,7 +3,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 def admin_main_menu():
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row(KeyboardButton("הפקדה מ־bit"))
-    markup.row(KeyboardButton("ביטול כל ההזמנות❌"), KeyboardButton("אספקה גורפת"))
+    markup.row(KeyboardButton("ביטול כל ההזמנות"), KeyboardButton("אספקה גורפת"))
     markup.row(KeyboardButton("בדיקת יתרות כוללת"), KeyboardButton("סיכום כללי"))
     markup.row(KeyboardButton("פקודות נוספות"), KeyboardButton("ניהול הזמנות"))
     return markup
