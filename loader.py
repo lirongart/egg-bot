c00f1174036be997b1971b3a3cb16f58de195ef8
lirongart <lirongart@gmail.com>
2025-04-22 00:22:24 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,4 +1,9 @@
 from telebot import TeleBot
+from flask import Flask
 from config import TOKEN
 
+# יצירת מופע הבוט
 bot = TeleBot(TOKEN)
+
+# יצירת מופע Flask (לשימוש אם אתה מפעיל Webhook)
+app = Flask(__name__)
