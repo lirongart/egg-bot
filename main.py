6c17ea6a7577438bfcef902a31db40865ed7b7fa
lirongart <lirongart@gmail.com>
2025-04-21 22:47:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import sys ,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import threading
+import logging
 from flask import Flask
 from telebot import TeleBot
 from config import TOKEN
 from handlers import user_commands, admin_commands, admin_broadcast, admin_supply_menu, admin_fulfill_exact_menu
-import threading
-import logging
-import os
 
 # === הגדרות לוגים בסיסיות ===
 logging.basicConfig(
