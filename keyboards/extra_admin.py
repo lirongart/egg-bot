54c5a8d6dfb6281710d5285b42553e7d2a382a93
lirongart <lirongart@gmail.com>
2025-04-24 02:08:42 +0300

    Update extra_admin.py

--- keyboards/extra_admin.py
+++ keyboards/extra_admin.py
@@ -4,11 +4,10 @@ def extra_admin_menu():
     markup = InlineKeyboardMarkup()
     markup.row(
         InlineKeyboardButton("📦 אספקה מדויקת", callback_data="cmd_fulfill_exact"),
-        InlineKeyboardButton("🔄 אספקה עם מידה שונה", callback_data="cmd_fulfill_alt")
+        InlineKeyboardButton("אספקה שונה", callback_data="cmd_fulfill_partial_menu")  # ← זה השם החדש
     )
     markup.row(
         InlineKeyboardButton("❌ ביטול הזמנה", callback_data="cmd_cancel"),
         InlineKeyboardButton("🆔 מה ה־ID שלי?", callback_data="cmd_me")
     )
     return markup
-
